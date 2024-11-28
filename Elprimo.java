dfa59957920302681bb106ba7a2241e72fb38373
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2024-11-28 14:49:17 +0100

    changes made

---
+++ Elprimo.java
@@ -0,0 +1,8 @@
+public class Elprimo {
+    final int var;
+    final String name;
+    Elprimo (final int var, final String name){
+        this.var=var;
+        this.name=name;
+    }
+}
---
+++ second.java
@@ -0,0 +1,12 @@
+public class second {
+    public second(long gui) {
+        this.gui = gui;
+    }
+
+    private final long gui;
+
+    public long getGui() {
+        return gui;
+    }
+    
+}
